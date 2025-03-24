5d207c54e80e01bcba713b9adc6ef720bf2fa876
Ev0lt <1815995014@qq.com>
2025-03-24 18:38:54 +0800

    v1.0

---
+++ README.md
@@ -0,0 +1,12 @@
+# Usage
+Usage: filelist.py <options> <object>
+
+Options:
+  -v, --version         查看版本号
+  -h, --help            查看帮助信息
+  -f FILENAME, --file=FILENAME
+                        指定查找文件 或者关键词
+  -d DIRECTORY, --directory=DIRECTORY
+                        指定查找目录如果没有指定那就是当前目录
+  -n NUM, --file_num=NUM
+                        指定只要查找几个文件
---
+++ filelist.py
@@ -0,0 +1,67 @@
+import os,optparse,threading
+all_file_path=[]
+def find_file_or_dir(path):
+    global all_file_path
+    try:
+        for i in os.listdir(path):
+            if os.path.isfile(path+"\\"+i):
+                all_file_path.append(path+'\\'+i)
+            else:
+                if os.path.isdir(path+'\\'+i):
+                    find_file_or_dir(path+"\\"+i)
+                else:
+                    all_file_path.append(path+'\\'+i)
+    except:
+        #print("没有权限访问:"+path+"\\"+i)
+        pass
+
+
+def main():
+    global all_file_path
+    threading_list=[]
+    version="%prog v1.0"
+    usage="%prog <options> <object>"
+    p=optparse.OptionParser(usage=usage,version=version)
+    p.add_option("-f","--file",dest="filename",help="指定查找文件 或者关键词")
+    p.add_option("-d","--directory",dest="directory",help="指定查找目录如果没有指定那就是当前目录",default=os.getcwd())
+    p.add_option("-n","--file_num",dest="num",help="指定只要查找几个文件",default="Infinity")
+    (option,args)=p.parse_args()
+    filename=option.filename
+    num=option.num
+    yx_path=r"%s"%option.directory
+    now_file=os.listdir(yx_path)
+    all_file_path=[]
+    if num != 'Infinity':
+        try:
+            num=int(num)
+        except:
+            exit("The num must number,Please enter the number.")
+    for i in now_file:
+        try:
+            if os.path.isfile(yx_path+'\\'+i):
+                all_file_path.append(yx_path+'\\'+i)
+            else:
+                path=yx_path+"\\"+i
+                t=threading.Thread(target=find_file_or_dir,args=(path,))
+                t.start()
+                threading_list.append(t)
+        except Exception as e:
+            #print("没有权限访问:"+path+"\\"+i)
+            #print(e)
+            pass
+    for i in threading_list:
+        i.join()
+    all_file_path=list(filter(lambda x:x.find(filename) >=0,all_file_path))
+    if num == 'Infinity':
+        for i in all_file_path:
+            print(i)
+    elif len(all_file_path) < num:
+        for i in all_file_path:
+            print(i)
+    else:
+        all_file_path=all_file_path[:num]
+        for i in all_file_path:
+            print(i)
+
+if __name__ == "__main__":
+    threading.Thread(target=main).start()
